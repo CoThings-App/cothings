afe3b04ad1e635fcaf397ac9c7a4b5f23667df67
Nesim TUNC <nesimtunc@gmail.com>
2020-04-15 21:00:29 +0900

    separated sockets for lobby and room

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -1,74 +1,76 @@
-import {Socket} from "phoenix"
+import { Socket } from "phoenix"
 import moment from './moment.min';
 
-let socket = new Socket("/socket", {params: {}})
+let socket = new Socket("/socket", { params: {} })
 
 socket.connect()
 
 const connectToTheRoom = (room_id) => {
 
-  let channel = socket.channel("lobby:" + room_id, {})
-  channel.join()
-    .receive("ok", resp => {
-        updateCounters(resp.room);
-    })
-    .receive("error", resp => { 
-      //TODO: put an alert on UI
-      console.error("Unable to join", resp)
-    })
+    let channel = socket.channel("lobby:" + room_id, {})
+    channel.join()
+        .receive("ok", resp => {
+            updateCounters(resp.room);
+        })
+        .receive("error", resp => {
+            //TODO: put an alert on UI
+            console.error("Unable to join", resp)
+        })
 
-  channel.on("enter", function(data) {
-    updateCounters(data.room);
-  });
-  channel.on("left", function(data) {
-    updateCounters(data.room);
-  });
-  
-  document.getElementById("btn-inc").addEventListener('click', () => {
-    channel.push("enter");
-  });
+    channel.on("enter", function(data) {
+        updateCounters(data.room);
+    });
+    channel.on("left", function(data) {
+        updateCounters(data.room);
+    });
 
-  document.getElementById("btn-dec").addEventListener('click', () => {
-    channel.push("left");
-  });
+    document.getElementById("btn-inc").addEventListener('click', () => {
+        channel.push("enter");
+    });
 
-  function updateCounters(room) {
-    const counter = document.getElementById("counter");
-    counter.innerText = `${room.count}/${room.limit}`;
-    counter.className = room.css_class;
-    document.getElementById("last-updated").innerText = moment.utc(room.last_updated).fromNow();
-    document.getElementById("percentage").innerText = `${room.percentage}%`;
-    document.getElementById("percentage-circle").className = `c100 p${room.percentage} ${room.css_class}`;
-  }
+    document.getElementById("btn-dec").addEventListener('click', () => {
+        channel.push("left");
+    });
+
+    function updateCounters(room) {
+        const counter = document.getElementById("counter");
+        counter.innerText = `${room.count}/${room.limit}`;
+        counter.className = room.css_class;
+        document.getElementById("last-updated").innerText = moment.utc(room.last_updated).fromNow();
+        document.getElementById("percentage").innerText = `${room.percentage}%`;
+        document.getElementById("percentage-circle").className = `c100 p${room.percentage} ${room.css_class}`;
+    }
 }
 
 const connectToTheLobby = () => {
 
-  let channel = socket.channel("lobby:*", {})
-  channel.join()
-    .receive("ok", resp => {
-        console.log(resp);
-        // if (!resp.room) return;
-    })
-    .receive("error", resp => {
-      //TODO: put an alert on UI
-      console.error("Unable to join", resp)
-    })
+    let channel = socket.channel("lobby:*", {})
+    channel.join()
+        .receive("ok", resp => {
+            console.log(resp);
+            // if (!resp.room) return;
+        })
+        .receive("error", resp => {
+            //TODO: put an alert on UI
+            console.error("Unable to join", resp)
+        })
 
-  channel.on("update", function(data) {
-    console.log("update", data);
-    updateTheRoomStats(data.room)
-  });
+    channel.on("update", function(data) {
+        console.log("update", data);
+        updateTheRoomStats(data.room)
+    });
 
-  function updateTheRoomStats(room) {
-    document.getElementById(`room_${room.id}_count`).innerText = room.count;
-    document.getElementById(`room_${room.id}_percentage`).innerText = `${room.percentage}%`;
-  }
+    function updateTheRoomStats(room) {
+        document.getElementById(`room_${room.id}_count`).innerText = room.count;
+        document.getElementById(`room_${room.id}_percentage`).innerText = `${room.percentage}%`;
+        document.getElementById(`bar_${room.id}`).classList.add(`${room.css_class}`);
+        document.getElementById(`bar_${room.id}`).style.width = `${room.percentage}%`;
+    }
 
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  moment.locale(window.navigator.userLanguage || window.navigator.language);
+    moment.locale(window.navigator.userLanguage || window.navigator.language);
 });
 
 window.connectToTheRoom = connectToTheRoom;
