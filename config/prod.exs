9f5d5b595158fc5f8a3790743b1b8919e005d7f1
Nesim TUNC <nesimtunc@gmail.com>
2020-04-16 09:57:02 +0900

    let app runs as a server also

--- config/prod.exs
+++ config/prod.exs
@@ -11,6 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :coliving, ColivingWeb.Endpoint,
   url: [host: "example.com", port: 80],
+  server: true,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
