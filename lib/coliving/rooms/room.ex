94f6c8820a6fb0c6e2bd9b17280aa02b82a529a4
Nesim TUNC <nesimtunc@gmail.com>
2020-04-20 18:34:37 +0900

    updated room schema for beacon related props

--- lib/coliving/rooms/room.ex
+++ lib/coliving/rooms/room.ex
@@ -2,12 +2,19 @@ defmodule Coliving.Rooms.Room do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @all_fields ~w(name count limit group ibeacon_uuid altbeacon_uuid major minor)a
+  @required_fields ~w(name count limit group)a
+
   schema "rooms" do
     field :name, :string
     # count of people / running machines etc.
     field :count, :integer
     field :limit, :integer
     field :group, :string
+    field :ibeacon_uuid, :string
+    field :altbeacon_uuid, :string
+    field :major, :integer
+    field :minor, :integer
 
     timestamps()
   end
@@ -15,7 +22,7 @@ defmodule Coliving.Rooms.Room do
   @doc false
   def changeset(room, attrs) do
     room
-    |> cast(attrs, [:name, :count, :limit, :group])
-    |> validate_required([:name, :count, :limit, :group])
+    |> cast(attrs, @all_fields)
+    |> validate_required(@required_fields)
   end
 end
