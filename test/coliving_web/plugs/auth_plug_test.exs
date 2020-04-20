ebb585684e491789ff280ca5bdd335dcd4ec09b5
Nesim TUNC <nesimtunc@gmail.com>
2020-04-20 13:33:57 +0900

    refactored plug tests

--- test/coliving_web/plugs/auth_plug_test.exs
+++ test/coliving_web/plugs/auth_plug_test.exs
@@ -5,22 +5,25 @@ defmodule ColivingWeb.AuthPlugTest do
 
   alias ColivingWeb.Router.Helpers
 
-  setup do
-    {:ok, conn: Phoenix.ConnTest.build_conn() }
-  end
-
-
-  test "user is redirected to login when try to manage rooms", %{ conn: conn } do
+  setup %{conn: conn} do
     conn =
       conn
-      |> with_pipeline()
+      |> bypass_through(ColivingWeb.Router, :browser)
+
+    {:ok, conn: conn}
+  end
+
+  test "user is redirected to login when try to manage rooms", %{conn: conn} do
+    conn =
+      conn
+      |> get(Helpers.room_path(conn, :index))
       |> ColivingWeb.Plugs.AuthPlug.call(%{})
 
     assert get_flash(conn, :error) == "You need to login in order to manage"
     assert redirected_to(conn) == Helpers.session_path(conn, :index)
   end
 
-  test "user passes auth when logged_in set", %{ conn: conn } do
+  test "user passes auth when logged_in set", %{conn: conn} do
     conn =
       conn
       |> assign(:logged_in, true)
@@ -28,10 +31,4 @@ defmodule ColivingWeb.AuthPlugTest do
 
     assert conn.status != 302
   end
-
-  defp with_pipeline(conn) do
-    conn |> bypass_through(ColivingWeb.Router, :browser)
-    |> get(Helpers.room_path(conn, :index))
-  end
-
 end
--- test/coliving_web/plugs/session_plug_test.exs
+++ test/coliving_web/plugs/session_plug_test.exs
@@ -3,21 +3,24 @@ defmodule ColivingWeb.SessionPlugTest do
 
   alias ColivingWeb.Router.Helpers
 
+  setup %{conn: conn} do
+    conn =
+      conn
+      |> bypass_through(ColivingWeb.Router, :browser)
 
-  setup do
-    {:ok, conn: Phoenix.ConnTest.build_conn() }
+    {:ok, conn: conn}
   end
 
-  test "conn will assign parameters if user has logged in" do
-    # conn = build_conn() |>
+  test "conn will assign parameters if user has logged in", %{conn: conn} do
+    conn = conn |> assign(:logged_in, true)
+    assert conn.assigns[:logged_in] == true
   end
 
   test "conn will remove assign parameters when user logouts", %{conn: conn} do
-    conn = conn |> bypass_through(ColivingWeb.Router, :browser)
-    |> get(Helpers.session_path(conn, :logout))
-    assert conn.assigns[:logged_id] == nil
+    conn =
+      conn
+      |> get(Helpers.session_path(conn, :logout))
+
+    assert conn.assigns[:logged_in] == nil
   end
-
-
-
 end
