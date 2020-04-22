53b87c2e0c5fefef815432afd52ce7f39cf2024c
Nesim TUNC <nesimtunc@gmail.com>
2020-04-22 11:32:58 +0900

    retrieve hostname from system env.

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :coliving, ColivingWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: System.get_env("HOST"), port: 80],
   server: true,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
