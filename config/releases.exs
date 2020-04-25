9af98bcc35526c6fa0c4822cc3da68974be6aa6a
Nesim TUNC <nesimtunc@gmail.com>
2020-04-25 14:28:05 +0900

    runtime configuration setup for docker image release

---
+++ config/releases.exs
@@ -0,0 +1,30 @@
+import Config
+
+database_url =
+  System.get_env("DATABASE_URL") ||
+    raise """
+    environment variable DATABASE_URL is missing.
+    For example: ecto://USER:PASS@HOST/DATABASE
+    """
+
+config :coliving, Coliving.Repo,
+  # ssl: true,
+  url: database_url,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+config :coliving, ColivingWeb.Endpoint,
+  http: [
+    port: String.to_integer(System.get_env("PORT") || "4000"),
+    transport_options: [socket_opts: [:inet6]]
+  ],
+  url: [host: System.get_env("HOST"), port: 80],
+  server: true,
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: secret_key_base
--- lib/coliving_web/endpoint.ex
+++ lib/coliving_web/endpoint.ex
@@ -1,49 +1,6 @@
 defmodule ColivingWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :coliving
 
-  @doc """
-  Callback invoked for dynamically configuring the endpoint.
-
-  It receives the endpoint configuration and checks if
-  configuration should be loaded from the system environment.
-  """
-  def init(_key, config) do
-    if config[:load_from_system_env] do
-      port =
-        Application.get_env(:coliving, :port) ||
-          raise "expected the PORT environment variable to be set"
-
-      secret_key_base =
-        Application.get_env(:coliving, :secret_key_base) ||
-          raise "expected the SECRET_KEY_BASE environment variable to be set"
-
-      host =
-        Application.get_env(:coliving, :host) ||
-          raise "expected the HOST environment variable to be set"
-
-      database_url =
-        Application.get_env(:coliving, :database_url) ||
-          raise """
-          expected the DATABASE_URL environment variable to be set
-          For example: ecto://USER:PASS@HOST/DATABASE
-          """
-
-      pool_size = String.to_integer(Application.get_env(:coliving, :pool_size) || "10")
-
-      config =
-        config
-        |> Keyword.put(:http, [:inet6, port: port])
-        |> Keyword.put(:secret_key_base, secret_key_base)
-        |> Keyword.put(:host, host)
-        |> Keyword.put(:database_url, database_url)
-        |> Keyword.put(:pool_size, pool_size)
-
-      {:ok, config}
-    else
-      {:ok, config}
-    end
-  end
-
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
