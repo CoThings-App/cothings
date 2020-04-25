5f7b124234796374d81444a9d69684785fac0e72
Nesim TUNC <nesimtunc@gmail.com>
2020-04-25 19:48:18 +0900

    configuring test env for GitLab CI

--- config/test.exs
+++ config/test.exs
@@ -15,6 +15,11 @@ if System.get_env("GITHUB_ACTIONS") do
     password: "postgres"
 end
 
+if System.get_env("GITLAB_CI") do
+  config :coliving, Coliving.Repo,
+  hostname: "postgres",
+end
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :coliving, ColivingWeb.Endpoint,
