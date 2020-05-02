dc27e51e44ce54969c7d58a6ed4b18dc4bc1910b
Nesim TUNC <nesimtunc@gmail.com>
2020-05-03 00:27:36 +0900

    rollback user_id from usages table

--- priv/repo/migrations/20200501141946_create_user_id_for_usages_and_notifications.exs
+++
@@ -1,9 +0,0 @@
-defmodule Coliving.Repo.Migrations.CreateUserIdForUsagesAndNotifications do
-  use Ecto.Migration
-
-  def change do
-    alter table(:usages) do
-      add :user_id, :binary_id
-    end
-  end
-end
