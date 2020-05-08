9a87b88aaff89c0b3a2f3d3546c014f14fbd52cd
Nesim TUNC <nesimtunc@gmail.com>
2020-05-08 19:35:09 +0900

    applying new desing

--- lib/coliving_web/views/page_view.ex
+++ lib/coliving_web/views/page_view.ex
@@ -1,3 +1,17 @@
 defmodule ColivingWeb.PageView do
   use ColivingWeb, :view
+
+  def get_room_percentage(room) do
+    round(room.count / room.capacity * 100)
+  end
+
+  def get_donut_percentage_array (room) do
+    per = get_room_percentage(room)
+    case per do
+       {100 } -> "100 0"
+        _ ->  "#{per} #{100 - per}"
+    end
+
+  end
+
 end
