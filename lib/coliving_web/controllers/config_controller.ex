5f6ff9bea5be688528d2eb5385b7cb1b26e20d35
Nesim TUNC <nesimtunc@gmail.com>
2020-05-15 22:59:04 +0900

    app related json config render

---
+++ lib/coliving_web/controllers/config_controller.ex
@@ -0,0 +1,14 @@
+defmodule ColivingWeb.ConfigController do
+  use ColivingWeb, :controller
+
+  def index(conn, _params) do
+    data = %{
+      "app" => %{
+        "title" => Application.get_env(:coliving, :app_title),
+        "image_url" => Application.get_env(:coliving, :app_image_url)
+      }
+    }
+    render(conn, "config.json", data: data)
+  end
+
+end
---
+++ lib/coliving_web/views/config_view.ex
@@ -0,0 +1,8 @@
+defmodule ColivingWeb.ConfigView do
+  use ColivingWeb, :view
+
+  def render("config.json", %{data: data }) do
+    data
+  end
+
+end
