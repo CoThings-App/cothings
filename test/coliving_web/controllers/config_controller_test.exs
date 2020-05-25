1da4cb6e09ea2a65c660f64c1a72a7307d261075
Nesim TUNC <nesimtunc@gmail.com>
2020-05-25 16:25:29 +0900

    config test

---
+++ test/coliving_web/controllers/config_controller_test.exs
@@ -0,0 +1,28 @@
+defmodule ColivingWeb.ConfigControllerTest do
+  use ColivingWeb.ConnCase
+
+  @env_app_title_value              "Test App"
+  @env_app_image_url_default_value  "http://localhost:4000/images/app_image.jpg"
+
+  setup %{conn: conn} do
+    # let's set only one variable
+    # we also test the string concation for the url
+    Application.put_env(:coliving, :app_title, @env_app_title_value)
+    {:ok, conn: conn}
+  end
+
+
+  test "GET /config.json returns pre defined and default values", %{conn: conn} do
+    conn = get(conn, "/config.json")
+
+    expected_config = %{
+      "app" => %{
+        "title" =>  @env_app_title_value,
+        "image_url" => @env_app_image_url_default_value
+      }
+    }
+
+    assert json_response(conn, 200) == expected_config
+  end
+
+end
